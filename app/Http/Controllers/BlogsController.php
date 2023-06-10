dc8ee8648b13394944706ce5dce465c6e44c96a3
Fogo Software <tuncaydemir682@gmail.com>
2023-06-10 20:45:37 +0300

    updated

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -40,7 +40,7 @@ class BlogsController extends Controller
     {
         $blog = Blogs::find($id);
 
-        return view('blogs.show', $blog);
+        return view('blogs.blogs-details', $blog);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/index', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 Route::get('/blogs', [BlogsController::class, 'index']);
-Route::get('/blogs/{id}', [BlogsController::class, 'show']);
+Route::get('/blogs/{id}', [BlogsController::class, 'blog-details']);
