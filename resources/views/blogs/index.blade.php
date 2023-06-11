9c4148ce4a806a0732720908e17017902c669620
Fogo Software <tuncaydemir682@gmail.com>
2023-06-11 16:39:02 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -7,17 +7,17 @@
         @if ($blog['active'])
 
             <div class="card my-4">
-                <div class="card-body">
+                <div>
                     <div class="row">
                         <div class="col-3">
-                            <img src="{{ asset('storage/img/Laravel.png') }}" alt="Image" class="img-fluid rounded-start" width="300" height="200">
+                            <img src="{{ asset('storage/img/Laravel.png') }}" alt="Image" class="img-fluid rounded-start" width="325" height="217">
                         </div>
                         <div class="col-9">
-                            <h4 class="card-title mb-4">{{ $blog['title'] }}</h4>
+                            <h4 class="card-title mb-3 mt-2">{{ $blog['title'] }}</h4>
                             <h6>{{ $blog['description'] }}</h6>
-                            <p>{{ $blog['likes'] }} likes</p>
+                            <p class="mb-1">{{ $blog['likes'] }} likes</p>
                             <p>{{ $blog['timestamp'] }}</p>
-                            <p>
+                            <p class="m-0">
                                 <a href="/blogs/{{ $blog['id'] }}" class="btn btn-danger px-5 py-2">Details</a>
                             </p>
                         </div>
