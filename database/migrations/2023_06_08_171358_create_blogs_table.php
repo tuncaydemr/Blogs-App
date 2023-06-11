395598a497e785698bad147a38a655924d919c7b
Fogo Software <tuncaydemir682@gmail.com>
2023-06-11 17:38:45 +0300

    updated

--- database/migrations/2023_06_08_171358_create_blogs_table.php
+++ database/migrations/2023_06_08_171358_create_blogs_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
+            $table->string('image');
             $table->integer('likes');
             $table->boolean('active');
             $table->timestamp('timestamp');
