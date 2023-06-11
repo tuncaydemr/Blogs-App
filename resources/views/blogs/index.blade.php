a1f9b897e295a83830a69ac26b5f274e8ef09336
Fogo Software <tuncaydemir682@gmail.com>
2023-06-11 16:28:01 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -8,14 +8,20 @@
 
             <div class="card my-4">
                 <div class="card-body">
-                    <img src="{{ asset('storage/img/Laravel.png') }}" alt="Image" class="img-fluid rounded">
-                    <h4 class="card-title mb-4">{{ $blog['title'] }}</h4>
-                    <h6>{{ $blog['description'] }}</h6>
-                    <p>{{ $blog['likes'] }} likes</p>
-                    <p>{{ $blog['timestamp'] }}</p>
-                    <p>
-                        <a href="/blogs/{{ $blog['id'] }}" class="btn btn-danger px-5 py-2">Details</a>
-                    </p>
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="{{ asset('storage/img/Laravel.png') }}" alt="Image" class="img-fluid rounded-start" width="300" height="200">
+                        </div>
+                        <div class="col-9">
+                            <h4 class="card-title mb-4">{{ $blog['title'] }}</h4>
+                            <h6>{{ $blog['description'] }}</h6>
+                            <p>{{ $blog['likes'] }} likes</p>
+                            <p>{{ $blog['timestamp'] }}</p>
+                            <p>
+                                <a href="/blogs/{{ $blog['id'] }}" class="btn btn-danger px-5 py-2">Details</a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
