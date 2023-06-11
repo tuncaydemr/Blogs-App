f77ccd43cd48134db27874f6af3bd6aec86a85bd
Fogo Software <tuncaydemir682@gmail.com>
2023-06-11 16:57:49 +0300

    updated

--- resources/views/blogs/blog-details.blade.php
+++ resources/views/blogs/blog-details.blade.php
@@ -7,13 +7,21 @@
                 <div class="col-3">
                     <img src="{{ asset('storage/img/Laravel.png') }}" alt="Image" class="img-fluid rounded-start" width="325" height="217">
                 </div>
-                <div class="col-9">
+                <div class="col-7">
                     <h4 class="card-title mb-3 mt-2">{{ $title }}</h4>
                     <h6>{{ $description }}</h6>
                     <p class="mb-1">{{ $likes }} likes</p>
                     <p>{{ $timestamp }}</p>
                     <p class="m-0">
                         <a href="/blogs" class="btn btn-danger px-5 py-2">Back</a>
+                    </p>
+                </div>
+                <div class="col-2 d-flex justify-content-center flex-column align-items-center">
+                    <p>
+                        <a href="/blogs" class="btn btn-primary px-4 py-2">Edit</a>
+                    </p>
+                    <p>
+                        <a href="/blogs" class="btn btn-primary px-3 py-2">Delete</a>
                     </p>
                 </div>
             </div>
