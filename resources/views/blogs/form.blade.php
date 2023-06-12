b962f0a845a0dd5861cac03aaec679fe4ea7c16c
Fogo Software <tuncaydemir682@gmail.com>
2023-06-12 20:33:35 +0300

    updated

---
+++ resources/views/blogs/form.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.layout')
+
+@section('content')
+
+    <form action="/submit-form" method="POST">
+        @csrf
+
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email">
+
+        <button type="submit">Submit</button>
+    </form>
+
+@endsection
