76ac4f2d4f49e2b45b5c295a3d1043bb748ed92f
Fogo Software <tuncaydemir682@gmail.com>
2023-06-15 20:45:29 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -21,7 +21,7 @@
                             <div class="like d-flex align-items-center">
                                 <p class="mb-4 me-3 mt-1" id="current-number">{{ $blog['likes'] }} likes</p>
 
-                                <button type="button" class="btn btn-primary py-1 mb-3">
+                                <button type="button" id="like-button" class="btn btn-primary py-1 mb-3">
                                     <i class="bi bi-hand-thumbs-up-fill" style="font-size: 0.9rem;"></i>
                                     Like
                                 </button>
