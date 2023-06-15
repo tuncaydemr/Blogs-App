f3206f6ed536d02213b17d5a6b4f9819fbb7ee74
Fogo Software <tuncaydemir682@gmail.com>
2023-06-15 22:17:23 +0300

    updated

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Like;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function like(Request $request)
+    {
+        $number = Like::find(4);
+
+        if ($number) {
+            // Increment the count
+            $number->likes++;
+            $number->save();
+        }
+
+        return response()->json(['blogs' => $number ? $number->count : null]);
+    }
 }
