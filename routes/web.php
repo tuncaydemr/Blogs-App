df414ff2b0685aa36da1ce5a83299aba08c7f14c
Fogo Software <tuncaydemir682@gmail.com>
2023-06-15 20:47:29 +0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ Route::get('/blogs/{id}', [BlogsController::class, 'show']);
 Route::get('/blogs/{id}/delete', [BlogsController::class, 'delete']);
 Route::get('/blogs/{id}/edit', [FormController::class, 'editForm']);
 Route::put('/blogs/{id}', [FormController::class, 'submitForm']);
-Route::post('/blogs', 'BlogsController@update');
+Route::post('/blogs', [BlogsController::class, 'update']);
