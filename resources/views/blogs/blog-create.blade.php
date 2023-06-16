1d11a1b8dc4ce6c7f4d595a6f71ef3621466114b
Fogo Software <tuncaydemir682@gmail.com>
2023-06-16 16:34:35 +0300

    updated

--- resources/views/blogs/blog-create.blade.php
+++ resources/views/blogs/blog-create.blade.php
@@ -6,7 +6,6 @@
         @csrf
 
         <div class="container d-flex justify-content-center align-items-center bg-danger rounded-4" style="height: 80vh;">
-
             <div class="row">
                 <div class="col-3">&nbsp;</div>
 
