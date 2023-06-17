5d9e2542891e61cce79fd3db879f77dd0720d6b2
Fogo Software <tuncaydemir682@gmail.com>
2023-06-17 13:54:29 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/index.js', 'resources/js/jquery-3.7.0.min.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
