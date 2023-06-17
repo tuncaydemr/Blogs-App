35714051a7e29e3c7921b53d0b2d7d9df9646ea4
Fogo Software <tuncaydemir682@gmail.com>
2023-06-17 15:15:14 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -22,10 +22,10 @@
                         <h6 class="mb-3">{{ $blog['description'] }}</h6>
                         @if ($blog['likes'])
                             <div class="like d-flex align-items-center">
-                                <p class="mb-4 me-3 mt-1"><span id="current-number">{{ $blog['likes'] }}</span> likes</p>
+                                <p class="mb-4 me-3 mt-1"><span>{{ $blog['likes'] }}</span> likes</p>
 
                                 <a href="/blogs/{{ $blog['id'] }}/like">
-                                    <button type="button" id="increment-button" class="btn btn-primary py-1 mb-3">
+                                    <button type="button" class="btn btn-primary py-1 mb-3">
                                         <i class="bi bi-hand-thumbs-up-fill" style="font-size: 0.9rem;"></i>
                                         Like
                                     </button>
