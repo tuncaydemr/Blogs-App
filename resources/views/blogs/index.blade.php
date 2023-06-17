ce2c0eea9da7c5c21dbe784fe4dff65a6161d88a
Fogo Software <tuncaydemir682@gmail.com>
2023-06-17 15:03:38 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -50,6 +50,7 @@
 
 @endsection
 
-@push('script')
-    <script src="js/index.js"></script>
+@push('scripts')
+    <script src="{{ asset('js/jquery-3.7.0.min.js') }}"></script>
+    <script src="{{ asset('js/index.js') }}"></script>
 @endpush
