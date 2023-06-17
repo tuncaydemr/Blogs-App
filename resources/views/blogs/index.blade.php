b88b6f4da1da16542288ba140ed872ad4f32b64d
Fogo Software <tuncaydemir682@gmail.com>
2023-06-17 19:19:46 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -40,7 +40,7 @@
                         @endif
                     </div>
                     <div class="col-3">
-                        <p class="m-0">
+                        <p>
                             <a href="/blogs/{{ $blog['id'] }}" class="btn btn-danger px-5 py-2">Details</a>
                         </p>
                     </div>
