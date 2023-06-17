fb3a657ddeb4d54ca55c789783af61a983932c22
Fogo Software <tuncaydemir682@gmail.com>
2023-06-17 19:34:20 +0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/index', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
-Route::get('/blogs', [BlogsController::class, 'blogs']);
-Route::get('/blogs/add', [BlogsController::class, 'add']);
+
 Route::get('/blogs/create', [FormController::class, 'create']);
-Route::get('/blogs/{id}', [BlogsController::class, 'show']);
+
 Route::get('/blogs/{id}/delete', [BlogsController::class, 'delete']);
 Route::get('/blogs/{id}/edit', [FormController::class, 'editForm']);
 Route::put('/blogs/{id}', [FormController::class, 'submitForm']);
 Route::get('/blogs/{id}/like', [BlogsController::class, 'update']);
+
+Route::controller(BlogsController::class)->group(function () {
+    Route::get('/blogs', 'blogs');
+    Route::get('/blogs/add', 'add');
+    Route::get('/blogs/{id}', 'show');
+});
