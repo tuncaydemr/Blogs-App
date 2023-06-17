3045f5296988ce12cd55735271ceaa70679f6568
Fogo Software <tuncaydemir682@gmail.com>
2023-06-17 15:49:07 +0300

    updated

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    @stack('styles')
+    @stack('cdn')
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-danger navbar-dark">
