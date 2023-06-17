653f57d4fe84d94dfae86506728d84b68fda3df6
Fogo Software <tuncaydemir682@gmail.com>
2023-06-17 15:45:14 +0300

    updated

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    @stack('styles')
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-danger navbar-dark">
