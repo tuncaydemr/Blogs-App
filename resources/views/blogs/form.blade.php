1a8b5400da68ae69904fbc38c37681776c6668ce
Fogo Software <tuncaydemir682@gmail.com>
2023-06-17 15:43:20 +0300

    updated

--- resources/views/blogs/form.blade.php
+++ resources/views/blogs/form.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 
-    <form action="/blogs/{{ $blog->id }}" method="POST" novalidate>
+    <form action="/blogs/{{ $id }}" method="POST" novalidate>
         @csrf
         @method('PUT')
 
@@ -16,11 +16,11 @@
             <div class="col-6">
                 <div class="mb-3">
                     <label for="title" class="form-label text-white">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{ $blog->title }}">
+                    <input type="text" class="form-control" id="title" name="title" value="{{ $title }}">
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label text-white">Description</label>
-                    <textarea class="form-control" name="description" id="description">{{ $blog->description }}</textarea>
+                    <textarea class="form-control" name="description" id="description">{{ $description }}</textarea>
                 </div>
                 <div class="mb-3">
                     <label for="image" class="form-label text-white">Image</label>
