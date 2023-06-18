f939fc4169f15328af01002a227f035527f1a00b
Fogo Software <tuncaydemir682@gmail.com>
2023-06-18 18:35:35 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -8,12 +8,7 @@
 
 
 
-    <div class="container-fluid">
-        <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-danger" type="submit">Search</button>
-        </form>
-    </div>
+
 
     <div class="container">
         <div class="row">
@@ -21,7 +16,12 @@
                 <a href="/blogs/add" class="btn btn-primary" role="button">Blog Add</a>
             </div>
             <div class="col-6">
-                
+                <div class="container-fluid">
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-danger" type="submit">Search</button>
+        </form>
+    </div>
             </div>
         </div>
     </div>
