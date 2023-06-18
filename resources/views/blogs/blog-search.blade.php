d64803ed3e9651e593f76758f54fdb00be48fbd3
Fogo Software <tuncaydemir682@gmail.com>
2023-06-18 19:24:41 +0300

    updated

--- resources/views/blogs/blog-search.blade.php
+++
@@ -1,9 +0,0 @@
-@if($results->isEmpty())
-                <p>No results found.</p>
-            @else
-                <ul>
-                    @foreach($results as $result)
-                        <li>{{ $result->title }}</li>
-                    @endforeach
-                </ul>
-            @endif
