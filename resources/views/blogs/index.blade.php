fc64e9f60288d0fd734e579132d023d240d1770d
Fogo Software <tuncaydemir682@gmail.com>
2023-06-18 18:37:13 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -14,11 +14,11 @@
             <div class="col-6">
                 <div class="container-fluid">
 
-                    <form class="d-flex" role="search">
+                    <form method="GET" class="d-flex" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-outline-danger" type="submit">Search</button>
                     </form>
-                    
+
                 </div>
             </div>
         </div>
