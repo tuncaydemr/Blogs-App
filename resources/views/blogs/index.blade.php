b93bc8e8bd6e39129ee7c13e299feeb8863ab6c3
Fogo Software <tuncaydemir682@gmail.com>
2023-06-18 18:35:42 +0300

    updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -17,11 +17,11 @@
             </div>
             <div class="col-6">
                 <div class="container-fluid">
-        <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-danger" type="submit">Search</button>
-        </form>
-    </div>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-danger" type="submit">Search</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
