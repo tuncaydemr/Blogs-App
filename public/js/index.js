34f55fb4356f200446c049a181068b2e097005f3
Fogo Software <tuncaydemir682@gmail.com>
2023-07-01 17:40:22 +0300

    Updated

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,12 @@
 $(() => {
-    let checkbox = $('#active');
-    let label = $('label[for="active"]');
+    let checkbox = $('#active')
+    let label = $('label[for="active"]')
 
     checkbox.on('change', function () {
         if (checkbox.is(':checked')) {
-            label.text('Disable');
+            label.text('Disable')
         } else {
-            label.text('Active');
+            label.text('Active')
         }
-    });
-
-    $("#sort-by-likes").on("change", () => {
-        let selectedOption = $(this).val();
-
-        $.ajax({
-            type: "GET",
-            url: "{{ route('sortByRate') }}",
-            data: { sort: selectedOption },
-            success: function (response) {
-                $("#sort-by-likes").html(response);
-            },
-        });
-    });
+    })
 })
