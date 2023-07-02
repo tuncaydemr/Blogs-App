b873362d63f63ed83cb805efb655418b757f207a
Fogo Software <tuncaydemir682@gmail.com>
2023-07-02 17:17:28 +0300

    Updated

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -8,6 +8,10 @@
     Contact
 @endsection
 
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+@endpush
+
 @push('cdn')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
