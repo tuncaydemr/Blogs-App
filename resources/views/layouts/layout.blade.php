63b2c89fa1dd75b618f9ecb1673def5e768167c3
Fogo Software <tuncaydemir682@gmail.com>
2023-07-02 18:26:26 +0300

    Updated

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -55,10 +55,6 @@
 
                             <div class="modal-body">
                                 <div class="mb-3">
-                                    <label for="name" class="form-label">Name</label>
-                                    <input type="text" class="form-control" name="name" id="name">
-                                </div>
-                                <div class="mb-3">
                                     <label for="email" class="form-label">Email</label>
                                     <input type="email" class="form-control" name="email" id="email">
                                 </div>
