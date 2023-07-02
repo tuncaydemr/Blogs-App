40141594e0969157e35da1858b193c730d1e1e69
Fogo Software <tuncaydemir682@gmail.com>
2023-07-02 18:26:09 +0300

    Updated

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -87,7 +87,7 @@
 
                             <div class="modal-body">
                                 <div class="mb-3">
-                                    <label for="name" class="form-label">Name</label>
+                                    <label for="name" class="form-label">Full Name</label>
                                     <input type="text" class="form-control" name="name" id="name">
                                 </div>
                                 <div class="mb-3">
