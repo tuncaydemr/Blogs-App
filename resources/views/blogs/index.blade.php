04acd5cc86c93c2a7e584d1d0d3d7a9c7eae46de
Fogo Software <tuncaydemir682@gmail.com>
2023-07-05 20:56:31 +0300

    Updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -35,7 +35,7 @@
                     <div class="col-12 col-md-5">
                         <img src="{{ asset('img/' . $blog['image']) }}" alt="Image" class="img-fluid rounded-start">
                     </div>
-                    <div class="col-8 col-md-4 d-flex flex-column justify-content-between card-body">
+                    <div class="col-8 col-md-4 d-flex flex-column card-body">
                         <h4 class="card-title">{{ $blog['title'] }}</h4>
                         <h6 class="card-description">{{ $blog['description'] }}</h6>
                         <div class="like d-flex align-items-center">
