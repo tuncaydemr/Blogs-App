6aae14d5ca70ea86e84314969aa89bd5e500f644
Fogo Software <tuncaydemir682@gmail.com>
2023-07-05 17:39:37 +0300

    Updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -32,23 +32,23 @@
 
             <div class="card my-4">
                 <div class="row">
-                    <div class="col-3">
+                    <div class="col-12 col-sm-3">
                         <img src="{{ asset('img/' . $blog['image']) }}" alt="Image" class="img-fluid rounded-start">
                     </div>
                     <div class="col-6 d-flex flex-column justify-content-between my-2">
-                        <h4 class="card-title mb-3">{{ $blog['title'] }}</h4>
-                        <h6 class="card-description mb-3">{{ $blog['description'] }}</h6>
+                        <h4 class="card-title mb-1">{{ $blog['title'] }}</h4>
+                        <h6 class="card-description mb-1">{{ $blog['description'] }}</h6>
                         <div class="like d-flex align-items-center">
 
                             @if($blog['likes'] > 1)
-                                <p class="mb-3 me-3">{{ $blog['likes'] }} likes</p>
+                                <p class=" me-3">{{ $blog['likes'] }} likes</p>
                             @elseif ($blog['likes'] <= 1)
                                 <p class="mb-3 me-3">{{ $blog['likes'] }} like</p>
                             @else
                                 <p class="mb-3">0 like</p>
                             @endif
 
-                            <a href="/blogs/{{ $blog['id'] }}/like">
+                            <a href="/blogs/{{ $blog['id'] }}/like" class="d-none">
                                 <button type="button" class="btn btn-primary py-1 mb-3">
                                     <i class="bi bi-hand-thumbs-up-fill" style="font-size: 0.9rem;"></i>
                                     Like
@@ -56,7 +56,7 @@
                             </a>
                         </div>
                     </div>
-                    <div class="col-3 d-flex align-items-center">
+                    <div class="col-3 d-flex align-items-center d-none">
                         <p>
                             <a href="/blogs/{{ $blog['id'] }}" class="btn btn-success px-5 py-2">Details</a>
                         </p>
