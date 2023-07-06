ee6c6293c88727ffd22c76d2b67f9fe326cbb0ed
Fogo Software <tuncaydemir682@gmail.com>
2023-07-06 17:56:50 +0300

    Updated

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -67,7 +67,7 @@
                                 </div>
 
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
                                     <button type="submit" class="btn btn-primary">Sign In</button>
                                 </div>
                             </form>
