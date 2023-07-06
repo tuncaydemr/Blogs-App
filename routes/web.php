ee2fa12408c9bfff2265b99cd6cfc942944c3d41
Fogo Software <tuncaydemir682@gmail.com>
2023-07-06 17:15:37 +0300

    Updated

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::controller(HomeController::class)->group(function () {
 
 Route::controller(BlogsController::class)->group(function () {
     Route::get('/blogs/index', 'blogs')->name('blogs');
-    Route::get('/blogs/add', 'add');
+    Route::get('/blogs/add', 'add')->name('blogs.add');
     Route::post('/blogs/create', 'create');
     Route::get('/blogs/{id}', 'show')->name('blogs.details');
     Route::get('/blogs/{id}/edit', 'edit')->name('blogs.edit');
