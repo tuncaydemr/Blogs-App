15ebbe046a080a66388f9112fc694f98f640bab8
Fogo Software <tuncaydemir682@gmail.com>
2023-07-06 18:05:12 +0300

    Updated

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::controller(HomeController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/index', 'index')->name('home');
+    Route::get('/home', 'home')->name('home');
     Route::get('/contact', 'contact')->name('contact');
 });
 
