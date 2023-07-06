d0ea081b565b55f6df719ad7f594fe5e9419900c
Fogo Software <tuncaydemir682@gmail.com>
2023-07-06 14:29:01 +0300

    Updated

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,28 @@
 $(() => {
-    let checkbox = $('#active')
-    let label = $('label[for="active"]')
+    let checkbox = $('#active');
+    let label = $('label[for="active"]');
 
     checkbox.on('change', function () {
         if (checkbox.is(':checked')) {
-            label.text('Disable')
+            label.text('Disable');
         } else {
-            label.text('Active')
+            label.text('Active');
         }
-    })
+    });
 
-    let navActive = $(".nav-link")
+    let navActive = $('.nav-link');
 
-    let currentPageUrl = window.location.href
+    let currentPageUrl = window.location.href;
 
     navActive.each(function () {
-        if ($(this).attr("href") === currentPageUrl) {
-            $(this).addClass("active border-bottom border-4")
+        if ($(this).attr('href') === currentPageUrl) {
+            $(this).addClass('active border-bottom border-4');
         }
-    })
+    });
 
-    if ($(".card-description h6").hasClass("text-truncate")) {
-        
+    if ($('.card-description h6').hasClass('text-truncate')) {
+        $('.card-description h6 a').addClass('d-block');
+    }else {
+        $('.card-description h6 a').addClass('d-block');
     }
-})
+});
