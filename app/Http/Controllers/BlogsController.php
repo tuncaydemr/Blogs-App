2224c884514fb8c4b12b3813db6c827d0376fdd8
Fogo Software <tuncaydemir682@gmail.com>
2023-07-06 18:10:29 +0300

    Updated

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -93,6 +93,6 @@ class BlogsController extends Controller
 
         Blogs::where('id', $id)->delete();
 
-        return redirect()->to('/blogs/index');
+        return redirect()->to('/blogs/home');
     }
 }
