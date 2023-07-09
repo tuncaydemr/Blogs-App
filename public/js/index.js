e8daa8f6f2ed59c11a0b6027f735687210552e91
Fogo Software <tuncaydemir682@gmail.com>
2023-07-09 19:31:33 +0300

    Updated

--- public/js/index.js
+++ public/js/index.js
@@ -27,12 +27,6 @@ $(() => {
         if (typeof text === "string") {
             let count = text.length;
             $("#letterCount").text(count + '/' + maxLength);
-
-            if (count > 60) {
-                $("#more").addClass("d-none");
-            } else {
-                $("#more").addClass("d-block");
-            }
         }
     });
 });
