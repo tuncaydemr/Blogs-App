d2451847b83f95cad1f47e22283b220eb53618fc
Fogo Software <tuncaydemir682@gmail.com>
2023-07-10 15:59:02 +0300

    Updated

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -54,13 +54,16 @@
                                     <div class="mb-3">
                                         <label for="email" class="form-label">Email</label>
                                         <input type="email" class="form-control" name="email" id="email">
-                                        @error('description')
+                                        @error('email')
                                             <div class="text-white mt-1">{{ ucwords($message) }}</div>
                                         @enderror
                                     </div>
                                     <div class="mb-3">
                                         <label for="pass" class="form-label">Password</label>
                                         <input type="password" class="form-control" name="pass" id="pass">
+                                        @error('pass')
+                                            <div class="text-white mt-1">{{ ucwords($message) }}</div>
+                                        @enderror
                                     </div>
                                 </div>
 
