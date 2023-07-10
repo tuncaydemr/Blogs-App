09ae6fa2b39ee169176b2fe2312989fdb3c79b26
Fogo Software <tuncaydemir682@gmail.com>
2023-07-10 17:29:20 +0300

    Updated

--- database/migrations/2023_07_10_140241_create_users_table.php
+++ database/migrations/2023_07_10_140241_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
-            $table->string('email');
+            $table->string('full_name')->unique();
+            $table->string('email')->unique();
             $table->string('password');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
