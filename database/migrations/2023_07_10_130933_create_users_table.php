7cd2f918b7b5b7ed4e207ff513c296ae40cd63c5
Fogo Software <tuncaydemir682@gmail.com>
2023-07-10 16:11:43 +0300

    Updated

--- database/migrations/2023_07_10_130933_create_users_table.php
+++ database/migrations/2023_07_10_130933_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->full_name();
-            $table->email();
-            $table->password();
+            $table->string('full_name');
+            $table->string('email');
+            $table->string('password');
         });
     }
 
