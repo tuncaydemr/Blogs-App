3d6825533758fa06207f5789fd9ed4c003848b27
Fogo Software <tuncaydemir682@gmail.com>
2023-07-11 19:44:55 +0300

    Updated

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -55,7 +55,7 @@ class BlogsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show($id)
+    public function show(int $id)
     {
         $blog = Blogs::findOrFail($id);
 
