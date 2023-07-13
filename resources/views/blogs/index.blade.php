ae8bd4eb8c9fe4942e6d5665f310d0339d7650e0
Fogo Software <tuncaydemir682@gmail.com>
2023-07-13 14:01:44 +0300

    Updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,76 +1,78 @@
-
-
 @extends('layouts.layout')
 
 @section('title')
     Blog App
 @endsection
 
-@section('content')
+@if (Session::has('user'))
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-2 my-2 p-0">
-                <a href="{{ route('blogs.add') }}" class="btn btn-primary d-block d-md-inline-block" role="button">Blog Add</a>
-            </div>
-            <div class="col-8 col-md-7 my-2 ps-0 pe-4 px-md-3">
-                <form method="GET" action="{{ route('search') }}" class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-danger" type="submit">Search</button>
-                </form>
-            </div>
-            <div class="col-4 col-md-3 my-2 p-0">
-                <select class="form-select" name="sortBy" id="sortBy">
-                    <option value="asc">Sort By</option>
-                    <option value="desc">Top Rated</option>
-                </select>
-            </div>
-        </div>
-    </div>
+    @section('content')
 
-    @foreach ($blogs as $blog)
-
-        @if ($blog['active'])
-
-            <div class="card my-4">
-                <div class="row">
-                    <div class="col-12 col-md-5 col-lg-4">
-                        <img src="{{ asset('img/' . $blog['image']) }}" alt="Image" class="img-fluid rounded-start">
-                    </div>
-                    <div class="col-8 col-md-4 col-lg-5 d-flex flex-column justify-content-evenly py-3 py-md-0">
-                        <h4 class="card-title">{{ $blog['title'] }}</h4>
-                        <div class="card-description">
-                            <h6 class="text-truncate" id="description">{{ $blog['description'] }}</h6>
-                            <a href="" class="more text-decoration-none" id="more">More...</a>
-                        </div>
-
-                        @if($blog['likes'] > 1)
-                            <p class="like">{{ $blog['likes'] }} likes</p>
-                        @elseif ($blog['likes'] === 1)
-                            <p class="like">{{ $blog['likes'] }} like</p>
-                        @else
-                            <p class="like">0 like</p>
-                        @endif
-
-                    </div>
-
-                    <div class="col-4 col-md-3 col-lg-3 d-flex flex-column align-items-center justify-content-center">
-                        <a href="{{ route('blogs.like', ['id' => $blog['id']]) }}">
-                            <button type="button" class="btn btn-primary py-1 mb-3">
-                                <i class="bi bi-hand-thumbs-up-fill"></i>
-                                Like
-                            </button>
-                        </a>
-                        <a href="{{ route('blogs.details', ['id' => $blog['id']]) }}" class="btn btn-success blogs-details-btn">Details</a>
-                    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-2 my-2 p-0">
+                    <a href="{{ route('blogs.add') }}" class="btn btn-primary d-block d-md-inline-block" role="button">Blog Add</a>
+                </div>
+                <div class="col-8 col-md-7 my-2 ps-0 pe-4 px-md-3">
+                    <form method="GET" action="{{ route('search') }}" class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-danger" type="submit">Search</button>
+                    </form>
+                </div>
+                <div class="col-4 col-md-3 my-2 p-0">
+                    <select class="form-select" name="sortBy" id="sortBy">
+                        <option value="asc">Sort By</option>
+                        <option value="desc">Top Rated</option>
+                    </select>
                 </div>
             </div>
+        </div>
 
-        @endif
+        @foreach ($blogs as $blog)
 
-    @endforeach
+            @if ($blog['active'])
 
-@endsection
+                <div class="card my-4">
+                    <div class="row">
+                        <div class="col-12 col-md-5 col-lg-4">
+                            <img src="{{ asset('img/' . $blog['image']) }}" alt="Image" class="img-fluid rounded-start">
+                        </div>
+                        <div class="col-8 col-md-4 col-lg-5 d-flex flex-column justify-content-evenly py-3 py-md-0">
+                            <h4 class="card-title">{{ $blog['title'] }}</h4>
+                            <div class="card-description">
+                                <h6 class="text-truncate" id="description">{{ $blog['description'] }}</h6>
+                                <a href="" class="more text-decoration-none" id="more">More...</a>
+                            </div>
+
+                            @if($blog['likes'] > 1)
+                                <p class="like">{{ $blog['likes'] }} likes</p>
+                            @elseif ($blog['likes'] === 1)
+                                <p class="like">{{ $blog['likes'] }} like</p>
+                            @else
+                                <p class="like">0 like</p>
+                            @endif
+
+                        </div>
+
+                        <div class="col-4 col-md-3 col-lg-3 d-flex flex-column align-items-center justify-content-center">
+                            <a href="{{ route('blogs.like', ['id' => $blog['id']]) }}">
+                                <button type="button" class="btn btn-primary py-1 mb-3">
+                                    <i class="bi bi-hand-thumbs-up-fill"></i>
+                                    Like
+                                </button>
+                            </a>
+                            <a href="{{ route('blogs.details', ['id' => $blog['id']]) }}" class="btn btn-success blogs-details-btn">Details</a>
+                        </div>
+                    </div>
+                </div>
+
+            @endif
+
+        @endforeach
+
+    @endsection
+
+@endif
 
 @push('styles')
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
