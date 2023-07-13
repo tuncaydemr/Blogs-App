c46b9ec6fb818c9bb3a52b97db3830650d740fa6
Fogo Software <tuncaydemir682@gmail.com>
2023-07-13 22:00:43 +0300

    Updated

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -51,7 +51,7 @@ class FormController extends Controller
 
             'email' => 'required|email|regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
 
-            'password' => 'required|confirmed|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/'
+            'password' => 'required|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/'
         ]);
 
         Users::insert([
