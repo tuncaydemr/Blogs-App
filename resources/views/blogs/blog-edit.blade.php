27c2b7f7ff6055df690bedcfc4674f30685424b1
Fogo Software <tuncaydemir682@gmail.com>
2023-07-16 09:07:47 +0300

    Updated

--- resources/views/blogs/blog-edit.blade.php
+++ resources/views/blogs/blog-edit.blade.php
@@ -4,39 +4,43 @@
     Blog Edit
 @endsection
 
-@section('content')
+@if (Session::has('user'))
 
-    <form action="{{ route('blogs.edit.submitForm', ['id' => $id]) }}" method="POST" novalidate enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
+    @section('content')
 
-        <div class="container d-flex justify-content-center align-items-center bg-dark rounded-4 mb-3" style="height: 80vh;">
-            <div class="row blog-edit">
-                <h2 class="mb-5 text-center text-white">Blog Edit</h2>
-                <div class="col-12">
-                    <div class="mb-3">
-                        <label for="title" class="form-label text-white">Title</label>
-                        <input type="text" class="form-control" id="title" name="title" value="{{ $title }}">
-                    </div>
-                    <div class="mb-3">
-                        <label for="description" class="form-label text-white">Description</label>
-                        <textarea class="form-control" name="description" id="description" maxlength="200">{{ $description }}</textarea>
-                        <p id="letterCount" class="text-white text-end"></p>
-                    </div>
-                    <div class="mb-3">
-                        <label for="image" class="form-label text-white">Image</label>
-                        <input class="form-control" type="file" id="image" name="image" accept=".png, .jpeg, .jpg">
-                        <input type="hidden" name="old_image" value="{{ $image }}">
-                    </div>
-                    <div class="w-100 d-flex justify-content-center mt-5">
-                        <button type="submit" class="btn btn-primary w-50 p-3">Edit</button>
+        <form action="{{ route('blogs.edit.submitForm', ['id' => $id]) }}" method="POST" novalidate enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+
+            <div class="container d-flex justify-content-center align-items-center bg-dark rounded-4 mb-3" style="height: 80vh;">
+                <div class="row blog-edit">
+                    <h2 class="mb-5 text-center text-white">Blog Edit</h2>
+                    <div class="col-12">
+                        <div class="mb-3">
+                            <label for="title" class="form-label text-white">Title</label>
+                            <input type="text" class="form-control" id="title" name="title" value="{{ $title }}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label text-white">Description</label>
+                            <textarea class="form-control" name="description" id="description" maxlength="200">{{ $description }}</textarea>
+                            <p id="letterCount" class="text-white text-end"></p>
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label text-white">Image</label>
+                            <input class="form-control" type="file" id="image" name="image" accept=".png, .jpeg, .jpg">
+                            <input type="hidden" name="old_image" value="{{ $image }}">
+                        </div>
+                        <div class="w-100 d-flex justify-content-center mt-5">
+                            <button type="submit" class="btn btn-primary w-50 p-3">Edit</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </form>
+        </form>
 
-@endsection
+    @endsection
+
+@endif
 
 @push('styles')
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
