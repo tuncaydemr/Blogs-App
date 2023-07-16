6099d5e7722b868c7327a6b5210b1a04d0c6ce31
Fogo Software <tuncaydemir682@gmail.com>
2023-07-16 09:27:43 +0300

    Updated

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+<body>
+    <h1>Welcome to Our Website</h1>
+    <p>Thank you for signing up. We are excited to have you!</p>
+</body>
+</html>
