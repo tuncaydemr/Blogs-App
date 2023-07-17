ae8f83a73dedb636c166ee6ce60e0457e31ce7a4
Fogo Software <tuncaydemir682@gmail.com>
2023-07-17 19:50:46 +0300

    Updated

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -14,7 +14,7 @@ class BlogsController extends Controller
      */
     public function blogs()
     {
-        $blogs = Blogs::simplePaginate(1);
+        $blogs = Blogs::orderBy('title')->simplePaginate(1);
 
         return view('blogs.index', ['blogs' => $blogs]);
     }
