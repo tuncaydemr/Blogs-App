c8d8ab0ce3c33fe24c76ec57b46c6f0a8abfa97f
Fogo Software <tuncaydemir682@gmail.com>
2023-07-18 15:30:33 +0300

    Updated

--- resources/views/account/my-account.blade.php
+++ resources/views/account/my-account.blade.php
@@ -13,7 +13,7 @@
 
             <div class="container d-flex justify-content-center align-items-center bg-dark rounded-4 mb-3" style="height: 90vh;">
                 <div class="row blog-create">
-                    <h2 class="mb-5 text-center text-white">Blog Create</h2>
+                    <h2 class="mb-5 text-center text-white">My Account</h2>
                     <div class="col-12">
                         <div class="mb-3">
                             <label for="title" class="form-label text-white">Title</label>
