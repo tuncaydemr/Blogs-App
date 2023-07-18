4d87375105f0b6dab46450e05af0cc95cb697ad2
Fogo Software <tuncaydemir682@gmail.com>
2023-07-18 15:48:53 +0300

    Updated

--- resources/views/account/my-account.blade.php
+++ resources/views/account/my-account.blade.php
@@ -8,41 +8,45 @@
 
     @section('content')
 
-        <form action="{{ route('my.account.edit') }}" method="POST" novalidate>
-            @csrf
+        @foreach ($users as $user)
 
-            <div class="container d-flex justify-content-center align-items-center bg-dark rounded-4 mb-3" style="height: 90vh;">
-                <div class="row my-account">
-                    <h2 class="mb-5 text-center text-white">My Account</h2>
-                    <div class="col-12">
-                        <div class="mb-3">
-                            <label for="username" class="form-label text-white">Username</label>
-                            <input type="text" class="form-control" id="username" name="username" value="{{ $users->username }}">
-                            @error('username')
-                                <div class="text-white mt-1">{{ ucwords($message) }}</div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label text-white">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" value="{{ $users->email }}">
-                            @error('email')
-                                <div class="text-white mt-1">{{ ucwords($message) }}</div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label text-white">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" value="{{ $users->password }}">
-                            @error('password')
-                                <div class="text-white mt-1">{{ ucwords($message) }}</div>
-                            @enderror
-                        </div>
-                        <div class="w-100 d-flex justify-content-center mt-5">
-                            <button type="submit" class="btn btn-primary w-50 p-3">Edit</button>
+            <form action="{{ route('my.account.edit') }}" method="POST" novalidate>
+                @csrf
+
+                <div class="container d-flex justify-content-center align-items-center bg-dark rounded-4 mb-3" style="height: 90vh;">
+                    <div class="row my-account">
+                        <h2 class="mb-5 text-center text-white">My Account</h2>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <label for="username" class="form-label text-white">Username</label>
+                                <input type="text" class="form-control" id="username" name="username" value="{{ $user->username }}">
+                                @error('username')
+                                    <div class="text-white mt-1">{{ ucwords($message) }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label text-white">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}">
+                                @error('email')
+                                    <div class="text-white mt-1">{{ ucwords($message) }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label text-white">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" value="{{ $user->password }}">
+                                @error('password')
+                                    <div class="text-white mt-1">{{ ucwords($message) }}</div>
+                                @enderror
+                            </div>
+                            <div class="w-100 d-flex justify-content-center mt-5">
+                                <button type="submit" class="btn btn-primary w-50 p-3">Edit</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
+
+        @endforeach
 
     @endsection
 
