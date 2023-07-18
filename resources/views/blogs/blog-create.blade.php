a1043fd7a3849ff816d5448891263fe76c54d256
Fogo Software <tuncaydemir682@gmail.com>
2023-07-18 13:55:25 +0300

    Updated

--- resources/views/blogs/blog-create.blade.php
+++ resources/views/blogs/blog-create.blade.php
@@ -58,6 +58,7 @@
 
 @push('styles')
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 @endpush
 
 @push('cdn')
