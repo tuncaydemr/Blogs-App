f8f69691277c12c1dbe5300a16a3a8203c0be1a1
Fogo Software <tuncaydemir682@gmail.com>
2023-07-18 15:28:43 +0300

    Updated

--- resources/views/account/my-account.blade.php
+++ resources/views/account/my-account.blade.php
@@ -7,6 +7,50 @@
 @if (Session::has('user'))
 
     @section('content')
+    
+        <form action="{{ route('blogs.create') }}" method="POST" novalidate enctype="multipart/form-data">
+            @csrf
+
+            <div class="container d-flex justify-content-center align-items-center bg-dark rounded-4 mb-3" style="height: 90vh;">
+                <div class="row blog-create">
+                    <h2 class="mb-5 text-center text-white">Blog Create</h2>
+                    <div class="col-12">
+                        <div class="mb-3">
+                            <label for="title" class="form-label text-white">Title</label>
+                            <input type="text" class="form-control" id="title" name="title">
+                            @error('title')
+                                <div class="text-white mt-1">{{ ucwords($message) }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label text-white">Description</label>
+                            <textarea class="form-control" name="description" id="description" maxlength="200"></textarea>
+                            <p id="letterCount" class="text-white text-end"></p>
+                            @error('description')
+                                <div class="text-white mt-1">{{ ucwords($message) }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label text-white">Image</label>
+                            <input class="form-control" type="file" id="image" name="image" accept=".png, .jpeg, .jpg">
+                            @error('image')
+                                <div class="text-white mt-1">{{ ucwords($message) }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="active" name="active">
+                            <label class="form-check-label text-white" for="active">Disable</label>
+                        </div>
+                            @error('active')
+                                <div class="text-white mt-1">{{ ucwords($message) }}</div>
+                            @enderror
+                        <div class="w-100 d-flex justify-content-center mt-5">
+                            <button type="submit" class="btn btn-primary w-50 p-3">Create</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
 
     @endsection
 
