6b9875d2f0bfe25c44d55d8ceb5e0f9294f4433d
Fogo Software <tuncaydemir682@gmail.com>
2023-07-20 18:29:05 +0300

    Updated

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -34,7 +34,7 @@
                         <img width="70" height="70" src="https://img.icons8.com/fluency/70/facebook-new.png" alt="facebook"/>
                     </a>
                     <a href="">
-                        <img width="70" height="70" src="https://img.icons8.com/fluency/70/linkedin.png" alt="linkedin"/>
+                        <img width="70" height="70" src="https://img.icons8.com/fluency/70/instagram-new.png" alt="instagram"/>
                     </a>
                     <a href="">
                         <img width="70" height="70" src="https://img.icons8.com/fluency/70/linkedin.png" alt="linkedin"/>
