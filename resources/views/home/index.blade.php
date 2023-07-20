246d1e632e7b07c65090b583e951a05eecd7c120
Fogo Software <tuncaydemir682@gmail.com>
2023-07-20 20:23:37 +0300

    Updated

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -53,10 +53,48 @@
         <div class="row">
             <div class="col">
 
-                @foreach ( as )
+                @foreach ($blogs as $blog)
+                
+                    @if ($blog['active'])
+
+                        <div class="card my-4">
+                            <div class="row">
+                                <div class="col-12 col-md-5 col-lg-4">
+                                    <img src="{{ asset('img/' . $blog['image']) }}" alt="Image" class="img-fluid rounded-start">
+                                </div>
+                                <div class="col-8 col-md-4 col-lg-5 d-flex flex-column justify-content-evenly py-3 py-md-0">
+                                    <h4 class="card-title">{{ $blog['title'] }}</h4>
+                                    <div class="card-description">
+                                        <h6 class="text-truncate" id="description">{{ $blog['description'] }}</h6>
+                                        <a href="" class="more text-decoration-none" id="more">More...</a>
+                                    </div>
+
+                                    @if($blog['likes'] > 1)
+                                        <p class="like">{{ $blog['likes'] }} likes</p>
+                                    @elseif ($blog['likes'] === 1)
+                                        <p class="like">{{ $blog['likes'] }} like</p>
+                                    @else
+                                        <p class="like">0 like</p>
+                                    @endif
+
+                                </div>
+
+                                <div class="col-4 col-md-3 col-lg-3 d-flex flex-column align-items-center justify-content-center">
+                                    <a href="{{ route('blogs.like', ['id' => $blog['id']]) }}">
+                                        <button type="button" class="btn btn-primary py-1 mb-3">
+                                            <i class="bi bi-hand-thumbs-up-fill"></i>
+                                            Like
+                                        </button>
+                                    </a>
+                                    <a href="{{ route('blogs.details', ['id' => $blog['id']]) }}" class="btn btn-success blogs-details-btn">Details</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    @endif
 
                 @endforeach
-                
+
             </div>
         </div>
     </div>
