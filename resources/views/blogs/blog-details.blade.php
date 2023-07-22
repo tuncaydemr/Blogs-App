cbfc3f1e24df048dc4d5b760f4f3a8faffed9cc6
Fogo Software <tuncaydemir682@gmail.com>
2023-07-22 16:18:10 +0300

    Updated

--- resources/views/blogs/blog-details.blade.php
+++ resources/views/blogs/blog-details.blade.php
@@ -8,52 +8,60 @@
 
     @section('content')
 
-        @if ($active)
+        <div class="container">
+            <div class="row">
+                <div class="col">
 
-            <div class="card my-4">
-                <div class="row">
-                    <div class="col-12 col-md-4">
-                            <img src="{{ asset('img/' . $image) }}" alt="Image" class="img-fluid rounded-start">
-                    </div>
-                    <div class="col-8 col-md-6 d-flex flex-column justify-content-evenly py-3 py-md-0">
-                        <h4 class="card-title">{{ $title }}</h4>
-                        <h6 class="card-description">{{ $description }}</h6>
+                    @if ($active)
 
-                        @if($likes > 1)
-                            <p class="like">{{ $likes }} likes</p>
-                        @elseif ($likes === 1)
-                            <p class="like">{{ $likes }} like</p>
-                        @else
-                            <p class="like">0 like</p>
-                        @endif
+                        <div class="card my-4">
+                            <div class="row">
+                                <div class="col-12 col-md-4">
+                                        <img src="{{ asset('img/' . $image) }}" alt="Image" class="img-fluid rounded-start">
+                                </div>
+                                <div class="col-8 col-md-6 d-flex flex-column justify-content-evenly py-3 py-md-0">
+                                    <h4 class="card-title">{{ $title }}</h4>
+                                    <h6 class="card-description">{{ $description }}</h6>
+
+                                    @if($likes > 1)
+                                        <p class="like">{{ $likes }} likes</p>
+                                    @elseif ($likes === 1)
+                                        <p class="like">{{ $likes }} like</p>
+                                    @else
+                                        <p class="like">0 like</p>
+                                    @endif
 
 
-                    </div>
-                    <div class="col-4 col-md-2 d-flex flex-column align-items-center justify-content-center">
-                        <p class="m-1">
-                            <a href="{{ route('blogs.edit', ['id' => $id]) }}" class="btn btn-primary px-4 py-2">Edit</a>
-                        </p>
-                        <p class="m-1">
-                            <a href="{{ route('blogs.delete', ['id' => $id]) }}" class="btn btn-danger px-3 py-2">Delete</a>
-                        </p>
-                    </div>
+                                </div>
+                                <div class="col-4 col-md-2 d-flex flex-column align-items-center justify-content-center">
+                                    <p class="m-1">
+                                        <a href="{{ route('blogs.edit', ['id' => $id]) }}" class="btn btn-primary px-4 py-2">Edit</a>
+                                    </p>
+                                    <p class="m-1">
+                                        <a href="{{ route('blogs.delete', ['id' => $id]) }}" class="btn btn-danger px-3 py-2">Delete</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12 d-flex justify-content-center py-3">
+                                    <a href="{{ route('blogs') }}" class="btn btn-primary px-5 py-2">Back</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    @else
+
+                        <div class="alert alert-warning">
+                            Blog Not Found
+                        </div>
+
+                    @endif
+
                 </div>
             </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 d-flex justify-content-center py-3">
-                        <a href="{{ route('blogs') }}" class="btn btn-primary px-5 py-2">Back</a>
-                    </div>
-                </div>
-            </div>
-
-        @else
-
-            <div class="alert alert-warning">
-                Blog Not Found
-            </div>
-
-        @endif
+        </div>
 
     @endsection
 
