8cf678ae40e7eb90bbbf04d5ab68f8788b027465
Fogo Software <tuncaydemir682@gmail.com>
2023-07-22 23:47:42 +0300

    Updated

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -205,7 +205,7 @@
             <div class="container-fluid bg-dark mt-5">
                 <div class="container">
                     <div class="row d-flex justify-content-between mb-5">
-                        <div class="col-5">
+                        <div class="col-5 d-flex flex-column justify-content-center">
                             <h4>About Me</h4>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, similique sunt blanditiis eos id ex inventore voluptate corrupti neque doloremque!</p>
                         </div>
