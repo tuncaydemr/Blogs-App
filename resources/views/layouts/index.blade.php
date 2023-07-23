51647a3198e07e301734f040080c04e03f4ef8c7
Fogo Software <tuncaydemir682@gmail.com>
2023-07-23 22:08:26 +0300

    Updated

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -106,6 +106,56 @@
                         </div>
                     </div>
                 </div>
+                <div id="login" class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false">
+                    <div class="modal-dialog modal-dialog-centered modal-fullscreen-sm-down">
+                        <div class="modal-content p-3">
+                            <form action="{{ route('login') }}" method="POST" novalidate>
+                                @csrf
+
+                                <div class="modal-header">
+                                    <h3>Login</h3>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" id="modalCloseButton"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="mb-3 form-group">
+                                        <label for="loginEmail" class="form-label">Email</label>
+                                        <input type="email" class="form-control
+                                            @error('loginEmail')
+                                                is-invalid
+                                            @enderror
+                                        " name="loginEmail" id="loginEmail">
+                                        @error('loginEmail')
+                                            <div class="text-danger p-2 rounded invalid-feedback">{{ ucfirst($message) }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="mb-3 form-group">
+                                        <label for="loginPassword" class="form-label">Password</label>
+                                        <input type="password" class="form-control
+                                            @error('loginPassword')
+                                                is-invalid
+                                            @enderror
+                                        " name="loginPassword" id="loginPassword">
+                                        @error('loginPassword')
+                                            <div class="invalid-feedback p-2 rounded">
+                                                <div class="text-danger">1- Minimum 8 characters.</div>
+                                                <div class="text-danger">2- At least one uppercase letter.</div>
+                                                <div class="text-danger">3- At least one lowercase letter.</div>
+                                                <div class="text-danger">4- At least one number.</div>
+                                                <div class="text-danger">5- At least one special character.</div>
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <a class="text-decoration-none me-auto" data-bs-toggle="modal" data-bs-target="#register">Register</a>
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" id="modalCloseButton2">Close</button>
+                                    <button type="submit" class="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 <div id="register" class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false">
                     <div class="modal-dialog modal-dialog-centered modal-fullscreen-sm-down">
                         <div class="modal-content p-3">
