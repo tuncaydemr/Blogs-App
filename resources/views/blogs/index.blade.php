287c8e4c981aa51e90e3b48400931e38a66292a8
Fogo Software <tuncaydemir682@gmail.com>
2023-07-23 23:39:09 +0300

    Updated

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -101,7 +101,7 @@
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="{{ asset('js/index.js') }}"></script>
 
-    @if (($userSession) || ($adminSession))
+    @if (!Session::has('user') || ($adminSession))
         <script>
             $(document).ready(function() {
                 openModal();
