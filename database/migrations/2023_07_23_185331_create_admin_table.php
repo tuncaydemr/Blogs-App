58aa9b37b4f1d9baaf83f6e35aaecfeee1a16a34
Fogo Software <tuncaydemir682@gmail.com>
2023-07-23 21:53:56 +0300

    Updated

--- database/migrations/2023_07_23_185331_create_admin_table.php
+++ database/migrations/2023_07_23_185331_create_admin_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('admin', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
         });
     }
 
