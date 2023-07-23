9f4441fce9146381b562e27625f971f796790c48
Fogo Software <tuncaydemir682@gmail.com>
2023-07-24 00:32:55 +0300

    Updated

--- resources/views/account/my-admin-account.blade.php
+++ resources/views/account/my-admin-account.blade.php
@@ -12,6 +12,13 @@
             @csrf
 
             <div class="container d-flex justify-content-center align-items-center bg-dark rounded-4 my-3" style="height: 90vh;">
+                <div class="row">
+                    <div class="col">
+                        @if (session('success'))
+                            <div class="alert alert-success">{{ session('success') }}</div>
+                        @endif
+                    </div>
+                </div>
                 <div class="row my-account">
                     <h2 class="mb-5 text-center text-white">My Admin Account</h2>
                     <div class="col-12">
