ebff50d6397a90f07b8b8b1fedc17c1a159967b6
Fogo Software <tuncaydemir682@gmail.com>
2023-07-23 23:15:14 +0300

    Updated

--- resources/views/blogs/blog-edit.blade.php
+++ resources/views/blogs/blog-edit.blade.php
@@ -4,7 +4,7 @@
     Blog Edit
 @endsection
 
-@if (Session::has('user') || Session::has('admin'))
+@if ($userSession || $adminSession)
 
     @section('content')
 
@@ -77,7 +77,7 @@
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="{{ asset('js/index.js') }}"></script>
 
-    @if (isset($userSession) || isset($adminSession))
+    @if ($userSession || $adminSession)
         <script>
             $(document).ready(function() {
                 openModal();
