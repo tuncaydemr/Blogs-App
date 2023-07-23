818987674620c3559f19fb8b2603d0a27346f768
Fogo Software <tuncaydemir682@gmail.com>
2023-07-24 00:58:08 +0300

    Updated

--- resources/views/account/my-admin-account.blade.php
+++ resources/views/account/my-admin-account.blade.php
@@ -17,6 +17,10 @@
                         @if (session('success'))
                             <div class="alert alert-success">{{ session('success') }}</div>
                         @endif
+
+                        @if (session('error'))
+                            <div class="alert alert-danger">{{ session('error') }}</div>
+                        @endif
                     </div>
                 </div>
                 <div class="row my-account">
