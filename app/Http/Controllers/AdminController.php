5163b97f3f054b385f0e477680d6ffc471e4963b
Fogo Software <tuncaydemir682@gmail.com>
2023-07-23 22:32:05 +0300

    Updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class AdminController extends Controller
 {
@@ -11,11 +13,17 @@ class AdminController extends Controller
         $username = $req->adminLoginUsername;
         $password = $req->adminLoginPassword;
 
-        $req->validate([
+        $admin = Admin::where('username', $username)->first();
+
+        $validator = $req->validate([
             'adminLoginUsername' => 'required|exists:admin,username',
             'adminLoginPassword' => 'required',
         ]);
 
+        if($admin && ($password === $username->password) && $validator) {
+            Session::put('admin', $admin);
+        }
 
+        return redirect()->to('/blogs/home');
     }
 }
