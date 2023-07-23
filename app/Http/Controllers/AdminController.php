62080cd335eab2b00edd6d640e1c369897bec815
Fogo Software <tuncaydemir682@gmail.com>
2023-07-23 22:26:14 +0300

    Updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function login(Request $req)
+    {
+        $username = $req->adminLoginUsername;
+        $password = $req->adminLoginPassword;
+
+        $req->validate([
+            'adminLoginUsername' => 'required',
+            'adminLoginPassword' => 'required',
+        ]);
+
+        
+    }
 }
